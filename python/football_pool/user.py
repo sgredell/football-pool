86e7c9ffcc3bf71e32be9cbe379b8d47a26d2a15
Steve Gredell <steve.gredell@milliman.com>
2017-08-17 00:25:38 -0400

    Create a User class
    Do this to begin authentication programming

---
+++ python/football_pool/user.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+
+# =============================================================================
+# LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
+# =============================================================================
+from flask_login import UserMixin
+
+
+class User(UserMixin):
+    """Defines a User"""
+    pass
